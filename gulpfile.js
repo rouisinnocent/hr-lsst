4446f38de779fb5beec6144601c663539f67aa2e
Plum <ezegarrido1@gmail.com>
2016-11-20 18:32:24 -0300

    Add more efficient clean/copy functionality.

--- gulpfile.js
+++ gulpfile.js
@@ -1,98 +1,140 @@
-var source = './src/';
-var dist = './dist/';
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
-var tsProject = ts.createProject('./config/tsconfig.json');
-var stylus = require('gulp-stylus');
+var ts_project = ts.createProject('config/tsconfig.json');
+var styl = require('gulp-stylus');
 var bsync = require('browser-sync').create();
 var del = require('del');
+var path = require('path');
+
+var source = 'src/';
+var dist = 'dist/';
+var index = 'html/index.html';
+var styl_files = source + 'styl/**/*.styl';
+var ts_files = source + 'ts/**/*.ts';
+var other_files = source + '**/!(*.styl|*.ts)';
+
+var routing = {
+    'styl': 'css',
+    'ts': 'js'
+}
+
+function compiler_error(err){
+    this.emit('end');
+    console.log('#'.repeat(20));
+    console.log(err);
+    console.log('#'.repeat(20));    
+}
+
 
 gulp.task('bsync:init', (done) => {
     bsync.init({
         server: {
-            baseDir: './dist',
-            index: 'html/index.html'
+            baseDir: dist,
+            index: index
         }
     });
     done();
 });
 
-gulp.task('reload', (done) => {
-    bsync.reload();
-    done();
-})
+gulp.task('compile:stylus', () => {
+    return gulp
+        .src(styl_files)
+        .pipe(styl())
+        .on('error',compiler_error)
+        .pipe(
+            gulp.dest(
+                dist + '/css'
+            )
+        );
+});
 
 gulp.task('compile:typescript', () => {
     return gulp
-        .src(source + '/ts/**/*.ts')
-        .pipe(tsProject())
+        .src(ts_files)
+        .pipe(ts_project())
         .js
+        .on('error',compiler_error)
         .pipe(
-        gulp.dest(dist + 'js/')
-        )
-});
-
-gulp.task('compile:stylus', () => {
-    return gulp
-        .src(source + 'stylus/**/*.styl')
-        .pipe(stylus())
-        .pipe(
-        gulp.dest(dist + 'css/')
-        )
-});
+            gulp.dest(
+                dist + '/js'
+            )
+        );
+ });
 
 gulp.task('copy', () => {
     return gulp
-        .src(
-        [
-            'src/**/*',
-            '!src/stylus/**/*',
-            '!src/stylus',
-            '!src/ts/**/*',
-            '!src/ts'
-        ]
-        )
+        .src([
+            source + '**/*',
+            '!' + source + 'styl/**/*',
+            '!' + source + 'styl',
+            '!' + source + 'ts/**/*',
+            '!' + source + 'ts',
+        ])
         .pipe(
         gulp.dest(dist)
         )
 });
 
-gulp.task('clean', () => {
-    return del(
-        [
-            'dist/**/*',
-            '!dist/css/**/*',
-            '!dist/css',
-            '!dist/js/**/**',
-            '!dist/js'
-        ]
-    );
+gulp.task('reload', (done) => {
+    bsync.reload();
+    done();
 });
 
 gulp.task('watch', (done) => {
-    gulp.watch(
-        source + 'ts/**/*.ts',
-        gulp.series('compile:typescript', 'reload')
-    );
-    gulp.watch(
-        source + 'stylus/**/*.styl',
-        gulp.series('compile:stylus', 'reload')
-    );
-    gulp.watch(
-        source + '**/!(*.ts|*.styl)',
-        gulp.series('clean', 'copy', 'reload')
-    );
+    var styl_watcher =
+        gulp.watch(
+            styl_files,
+            gulp.series(
+                'compile:stylus',
+                'reload'
+            )
+        );
+    var ts_watcher =
+        gulp.watch(
+            ts_files,
+            gulp.series(
+                'compile:typescript',
+                'reload'
+            )
+        );
+    var other_watcher =
+        gulp.watch(
+            other_files,
+            gulp.series(
+                'copy',
+                'reload'
+            )
+        );
+    styl_watcher.on('unlink', delete_file);
+    ts_watcher.on('unlink', delete_file);
+    other_watcher.on('unlink', delete_file);
     done();
-})
+});
 
-
+function delete_file(file_path) {
+    var dir = path.relative(source, file_path);
+    dir = path.dirname(dir);
+    var file_ext = path.extname(file_path);
+    var file_name = path.basename(file_path, file_ext);
+    for (let route in routing) {
+        let routed = routing[route];
+        file_ext = file_ext.replace(route, routed);
+        dir = dir.replace(route, routed);
+    }
+    var file_base = file_name + file_ext;
+    var dist_path = path.resolve(dist, dir, file_base);
+    return del(dist_path);
+}
 
 gulp.task(
     'default',
     gulp.series(
         'bsync:init',
         'watch',
-        'clean',
-        gulp.parallel('compile:typescript', 'compile:stylus', 'copy')
+        gulp.parallel(
+            'compile:stylus',
+            'compile:typescript'
+        ),
+        'reload'
     )
-);
+)
